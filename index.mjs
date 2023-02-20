9505ebd97f0be08cf3514c0ad656ac32176bad9f
rawpixel-vincent <vincent@rawpixel.com>
2023-02-20 13:30:43 +0700

    update zipper options, code bundler

---
+++ index.mjs
@@ -0,0 +1,6 @@
+import { zipper } from './zipper.mjs';
+import { validateZipperOptions, waitUntilValueMatch } from './utils.mjs';
+export default {
+  zipper,
+  utils: { validateZipperOptions, waitUntilValueMatch },
+};
